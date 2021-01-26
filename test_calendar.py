c604230d308064056d6d5b6ee82ac79f4cead70e
chisevew <chisevew@gmail.com>
2021-01-26 10:45:43 +0900

    FastAPI-study

--- test_calendar.py
+++ test_calendar.py
@@ -1,8 +1,15 @@
 import calendar
 import datetime
+import os
 
 today = datetime.date.today()
 theyear = today.year
 themonth = today.month
-print(calendar.month(theyear, themonth))
-gi
+
+html_c = calendar.HTMLCalendar()
+
+# 保存するフォルダ/カレンダーのファイル名.htmlで記入↓
+path = ""
+
+with open(path, mode='w') as f:
+    f.write(html_c.formatmonth(theyear, themonth, withyear=True))
