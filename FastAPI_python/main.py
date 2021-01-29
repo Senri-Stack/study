b2aff2631e3109925588c1df53c7b56e19f85a4d
chisevew <chisevew@gmail.com>
2021-01-29 10:46:42 +0900

    fastapi_python

--- FastAPI_python/main.py
+++ FastAPI_python/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI, HTTPException
+from typing import Optional, Set
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-items = {"foo": "The Foo Wrestlers"}
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: Set[str] = []
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str):
-    if item_id not in items:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"item": items[item_id]}
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+)
+async def create_item(item: Item):
+    return item
